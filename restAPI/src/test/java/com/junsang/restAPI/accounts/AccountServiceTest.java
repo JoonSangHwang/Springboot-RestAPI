a59047175dbee08be9a7a30338b189bf0ebf96d4
HJS <joonsang@lotte.net>
2020-12-29 17:04:35 +0900

    [2020.12.29] 예외 테스트

--- restAPI/src/test/java/com/junsang/restAPI/accounts/AccountServiceTest.java
+++ restAPI/src/test/java/com/junsang/restAPI/accounts/AccountServiceTest.java
@@ -37,6 +37,10 @@ public class AccountServiceTest {
     @Autowired
     AccountRepository accountRepository;
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+
     @Test
     public void findByUsername() {
         // Given
@@ -65,6 +69,47 @@ public class AccountServiceTest {
         // Then
         //= 사용자가 입력한 패스워드와 UserDetailsService 인터페이스에서 읽어 들인 패스워드 비교 (패스워드 인코더 사용)
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+
+    @Test(expected = UsernameNotFoundException.class)
+    @TestDescription("조회 한 Username 이 존재하지 않을때")
+    public void findByUsernameFail_1() {
+        // Expected
+        String username = "random@gmail.com";
+
+        // When
+        accountService.loadUserByUsername(username);
+    }
+
+
+    @Test
+    @TestDescription("조회 한 Username 이 존재하지 않을때")
+    public void findByUsernameFail_2() {
+        // Expected
+        String username = "random@gmail.com";
+
+        // When
+        try {
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed");          // 테스트를 실패하게 만듬 -> catch
+        } catch (UsernameNotFoundException e) {
+            // 에러메시지가 username 을 가지고 있는지 확인 (에러 객체를 가지고 있어 많은걸 확인 가능)
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+    }
+
+
+    @Test
+    @TestDescription("조회 한 Username 이 존재하지 않을때")
+    public void findByUsernameFail_3() {
+        // Expected
+        String username = "random@gmail.com";
+        expectedException.expect(UsernameNotFoundException.class);  // 예외가 나오기 전에 선언 해줘야함 !
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
     }
 
 }
